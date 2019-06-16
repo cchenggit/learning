30582d2965ed5a42a84ea90743be7ba00837de3d
cchenggit <654031023@qq.com>
2019-06-16 21:07:11 +0800

    add docs

--- design-pattern/src/main/java/com/cchenggit/desgin/singleton/ThreadSafeLazySingleton.java
+++ design-pattern/src/main/java/com/cchenggit/desgin/singleton/ThreadSafeLazySingleton.java
@@ -1,8 +1,11 @@
 package com.cchenggit.desgin.singleton;
 
+/**
+ * 通过给方法加锁的方式，但是效率低，不推荐使用。
+ */
 public class ThreadSafeLazySingleton {
 
-    private static volatile ThreadSafeLazySingleton threadSafeLazySingleton;
+    private static ThreadSafeLazySingleton threadSafeLazySingleton;
 
     /**
      * 这种方式是线程安全，每次获取该单例的时候，都通过synchronized关键字加锁，从而实现了线程安全
